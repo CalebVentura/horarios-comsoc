5a728682f651112eb7908b9ba135bf77281f6e1f
CalebVentura <caleb.ventura.v@uni.pe>
2022-03-29 14:59:34 -0500

    horarios añadidos

--- index.js
+++ index.js
@@ -36,17 +36,34 @@ const generarObjCursos = (dataText) => {
 
         // Para visualizar el contenido de cada curso (linea)
         const contenidoCurso = dataText.split(arrayCodCursos[i])[1].split(arrayCodCursos[i + 1])[0].trim()
-        // console.log(contenidoCurso)
 
         // Al arreglo arrayCursos le añadimos objetos que contengan los datos de cada curso
+        const cod = arrayCodCursos[i]
+        const horario = (() => {
+            // const hor = contenidoCurso.match(/\d{2}[.:]\d{2}\s?[-]\s?\d{2}[.:]\d{1,2}/g)
+            const hor = contenidoCurso.match(/\d{2}[:]/g)
+            if (hor !== null) return hor.map(h => h.slice(0,-1))
+            else {
+                // console.log(contenidoCurso)
+                return ''
+            }
+        })()
+        // console.log(horario)
+
+        // En caso de que no encuentre horarios
+        //if(typeof horario !== 'object') console.log(cod + ' : ' + horario)
+
         arrayCursos.push({
-            cod: arrayCodCursos[i],
+            cod,
+            horario,
             contenidoCurso,
         })
 
+        dataText = dataText.replace(arrayCodCursos[i], '').replace(contenidoCurso, '')
+
         //...Por mejorar
     }
-    // console.log(arrayCursos)
+    return arrayCursos
 }
 
 // Ejecución secuencial de funciones
@@ -62,5 +79,6 @@ const generarObjCursos = (dataText) => {
     });
 
     // Generar el objeto de cursos
-    generarObjCursos(dataText)
+    const arrayCursos = generarObjCursos(dataText)
+    console.log(arrayCursos)
 })()
